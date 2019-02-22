6a599fc52c038e8421fa930b1fa9734b741666a3
caipinfang <caipinfang@meituan.com>
2019-02-22 10:27:23 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+// import React, { Component } from 'react';
+// console
+//     .log(
+//     'hello' +
+//         "my" +
+// "friend"
+//     )
+// ;
+// const test = 'hello world';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>hello</div>
+//     )
+//   }
+// }
+
+/** ********** programmer error */
+
+// const foo = bar;
+
+// for (let i = 0; i < 10; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i >= 0; i++) {
+//   console.log(i);
+// }
+
+/** ********** best practices */
+
+// if ([] == false) {
+//   console.log('111');
+// } else {
+//   console.log('222');
+// }
+// if (3 == '03') {
+//   console.log('111');
+// } else {
+//   console.log('222');
+// }
+
+/** ********** style issue */
+// console
+//     .log(
+//     'hello' +
+//         "my" +
+// "friend"
+//     )
+// ;
+
+
+/** ********** rules modify example */
+
+// const bar = 10;
+// const work = Promise((resolve, reject) => {
+//   resolve('Some data');
+// });
+// work.then((data) => {
+//   console.log(data);
+// });
+
+// class App extends Component {
+//   componentDidMount() {
+//     console.log('test');
+//   }
+
+//   render() {
+//     return <div>hello</div>;
+//   }
+// }
+// export default App;
